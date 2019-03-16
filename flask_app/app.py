14142ac03ead5cd73ded4d575bc82d7d4c729fea
Justin Yu <justinyu1618@gmail.com>
2019-03-16 17:48:01 -0400

    fixing heroku dependencies

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,6 +3,10 @@ from helper import get_similar, get_sentences
 
 app = Flask(__name__)
 
+@app.route('/')
+def error(word):
+	return "No word specified. format: https://FULL_PATH/<word>"
+
 @app.route('/<word>')
 def index(word):
 	return jsonify(get_sentences(word))
